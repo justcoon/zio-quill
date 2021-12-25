4e532e7e52e3437ce21d25ff68cc47902483521a
Peter Kotula <peto.kotula@yahoo.com>
2021-12-26 00:50:22 +0100

    cassandra alpakka

--- quill-cassandra-alpakka/src/test/scala/io/getquill/context/cassandra/alpakka/CassandraAlpakkaSpec.scala
+++ quill-cassandra-alpakka/src/test/scala/io/getquill/context/cassandra/alpakka/CassandraAlpakkaSpec.scala
@@ -1,18 +1,22 @@
 package io.getquill.context.cassandra.alpakka
 
 import akka.actor.ActorSystem
+import akka.stream.Materializer
 import akka.stream.alpakka.cassandra.CassandraSessionSettings
 import akka.stream.alpakka.cassandra.scaladsl.{ CassandraSessionRegistry, CassandraSession => CassandraAlpakkaSession }
 import akka.testkit.TestKit
 import io.getquill.context.cassandra.CassandraTestEntities
 import io.getquill.{ CassandraAlpakkaContext, Literal, Spec }
 
+import scala.concurrent.ExecutionContext
+
 trait CassandraAlpakkaSpec extends Spec {
 
   val actorSystem: ActorSystem = ActorSystem("test")
   val alpakkaSession: CassandraAlpakkaSession = CassandraSessionRegistry.get(actorSystem).sessionFor(CassandraSessionSettings())
 
-  implicit val executionContext = actorSystem.dispatcher
+  implicit val materializer: Materializer = Materializer.matFromSystem(actorSystem)
+  implicit val executionContext: ExecutionContext = actorSystem.dispatcher
 
   lazy val testDB: CassandraAlpakkaContext[Literal.type] with CassandraTestEntities = new CassandraAlpakkaContext(Literal, alpakkaSession, actorSystem.settings.config.getInt("testDB.preparedStatementCacheSize")) with CassandraTestEntities
 
--- quill-cassandra-alpakka/src/test/scala/io/getquill/context/cassandra/alpakka/PeopleCassandraSpec.scala
+++ quill-cassandra-alpakka/src/test/scala/io/getquill/context/cassandra/alpakka/PeopleCassandraSpec.scala
@@ -1,5 +1,6 @@
 package io.getquill.context.cassandra.alpakka
 
+import akka.stream.scaladsl.Sink
 import io.getquill._
 
 class PeopleCassandraSpec extends CassandraAlpakkaSpec {
@@ -8,14 +9,15 @@ class PeopleCassandraSpec extends CassandraAlpakkaSpec {
 
   case class Person(id: Int, name: String, age: Int)
 
+  val entries = List(
+    Person(1, "Bob", 30),
+    Person(2, "Gus", 40),
+    Person(3, "Pet", 20),
+    Person(4, "Don", 50),
+    Person(5, "Dre", 60)
+  )
+
   override def beforeAll = {
-    val entries = List(
-      Person(1, "Bob", 30),
-      Person(2, "Gus", 40),
-      Person(3, "Pet", 20),
-      Person(4, "Don", 50),
-      Person(5, "Dre", 60)
-    )
     await {
       testDB.run(query[Person].delete)
     }
@@ -25,13 +27,57 @@ class PeopleCassandraSpec extends CassandraAlpakkaSpec {
     ()
   }
 
-  val q = quote {
+  val qByIds = quote {
     (ids: Query[Int]) => query[Person].filter(p => ids.contains(p.id))
   }
 
+  val q = quote {
+    query[Person]
+  }
+
   "Contains id" - {
-    "empty" in {
-      testDB.run(q(liftQuery(Set.empty[Int]))).map(res => res mustEqual List.empty[Person])
+    "query empty" in {
+      await {
+        testDB.run(qByIds(liftQuery(Set.empty[Int]))).map(res => res mustEqual List.empty[Person])
+      }
+    }
+
+    "stream empty" in {
+      await {
+        testDB.stream(qByIds(liftQuery(Set.empty[Int]))).runWith(Sink.seq).map(res => res mustEqual Seq.empty[Person])
+      }
+    }
+
+    val evenEntries = entries.filter(e => e.id % 2 == 0).toSet
+    val evenIds = evenEntries.map(_.id)
+
+    "query" in {
+      await {
+        testDB.run(qByIds(liftQuery(evenIds))).map(res => res.toSet mustEqual evenEntries)
+      }
+    }
+
+    "stream" in {
+      await {
+        testDB.stream(qByIds(liftQuery(evenIds))).runWith(Sink.seq).map(res => res.toSet mustEqual evenEntries)
+      }
+      await {
+        testDB.stream(q).filter(e => evenIds.contains(e.id)).runWith(Sink.seq).map(res => res.toSet mustEqual evenEntries)
+      }
+    }
+  }
+
+  "All" - {
+    "query" in {
+      await {
+        testDB.run(q).map(res => res.toSet mustEqual entries.toSet)
+      }
+    }
+
+    "stream" in {
+      await {
+        testDB.stream(q).runWith(Sink.seq).map(res => res.toSet mustEqual entries.toSet)
+      }
     }
   }
 }
